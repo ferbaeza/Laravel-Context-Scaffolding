1c098f54be03c9872be800f2c331455015fb79ff
Fer Baeza <fbaezahurtado@gmail.com>
2024-03-20 21:15:49 +0000

    package

--- src/script.php
+++ src/script.php
@@ -1,10 +1,10 @@
 <?php
 
-$path(dirname(dirname(__DIR__)));
+$path = (dirname(dirname(__DIR__)));
 $dao = '/src/Shared/Dao';
 
-if (mkdir($path . $dao , 0755, true) === false) {
-    $this->error('Error al crear la carpeta: ' . $path.$dao);
+if (mkdir($path . $dao , 0777, true) === false) {
+    echo('Error al crear la carpeta: ' . $path.$dao);
     return false;
 }
 
